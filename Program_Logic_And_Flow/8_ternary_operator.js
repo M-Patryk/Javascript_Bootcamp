c189b14e90d0908bfbe6763b154f37a2f3bb4415
PatrykM <patrykxmajchrzak@gmail.com>
2020-09-05 21:16:51 +0200

    switch and ternary operator

---
+++ Program_Logic_And_Flow/8_ternary_operator.js
@@ -0,0 +1,11 @@
+// So basically we have a condition and true and false outcomes. If the condition is true it prints true condition, if false then false.
+
+let number = 3;
+
+number === 7 ? console.log("Number matches the condition") :console.log("Number doesn't match the condition!");
+
+
+// Other showcase of ternary operator
+let status = 'online';
+let color = status === 'offline' ? 'red' : 'green';
+console.log(color)
