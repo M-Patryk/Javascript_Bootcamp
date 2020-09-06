638ef2fdd32232c3c352e5e7adc12bce8745f282
PatrykM <patrykxmajchrzak@gmail.com>
2020-09-06 18:12:39 +0200

    creating and reading arrays

---
+++ Arrays/creating_arrays.js
@@ -0,0 +1,10 @@
+let nums = [1,2,3,4,5];
+let shoppingList = ['tomato', 'potato', 'books', 'paper']
+
+console.log(nums, shoppingList)
+
+
+console.log(shoppingList.length, '.length')
+console.log(shoppingList[2], "index 2")
+console.log(shoppingList[-1], "index -1")
+console.log(nums[nums.length-1], 'method to get the last one')
