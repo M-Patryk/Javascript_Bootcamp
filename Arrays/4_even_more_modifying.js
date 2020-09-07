420a1c91f40379a126985e27a7c69ee0382e1c35
PatrykM <patrykxmajchrzak@gmail.com>
2020-09-07 21:43:15 +0200

    added sort method

--- Arrays/4_even_more_modifying.js
+++ Arrays/4_even_more_modifying.js
@@ -7,7 +7,6 @@ console.log(swimmers);
 let walkers = animals.slice(3);
 console.log(walkers);
 
-
 // ["shark", "salmon", "prawn", "tortoise", "giraffe", "rhino"]
 let fromBackwards = animals.slice(-4, -1);
 console.log(fromBackwards, 'this is with negative number');
@@ -17,15 +16,22 @@ console.log(animals);
 animals.splice(1, 0, 'octopus', 'snake');
 console.log(animals, 'this is after adding octopus and snake');
 
-
 // now removing snake ["shark", "octopus", "snake", "salmon", "prawn", "tortoise", "giraffe", "rhino"]
 animals.splice(2, 1);
 console.log(animals);
 
 //removing multiple
-animals.splice(0,3)
-console.log(animals)
+animals.splice(0, 3);
+console.log(animals);
 
 //replacing items
-animals.splice(0, 2, 'whale', 'dolphin')
-console.log(animals)
+animals.splice(0, 2, 'whale', 'dolphin');
+console.log(animals);
+
+//sort mozna sortowac alfabetycznie, ale liczb juz tym nie sortujemy
+let sortedAnimals = animals.sort();
+console.log(sortedAnimals);
+
+let nums = [ 3, 12, 1, 66, 100000, 777, 45 ];
+let sortedNums = nums.sort();
+console.log(sortedNums);
