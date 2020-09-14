6b7a0767a7d0fe7e1f52646d67c19b943a7e8ab1
PatrykM <patrykxmajchrzak@gmail.com>
2020-09-14 21:06:24 +0200

    for loops

---
+++ Loops/1_for_loop.js
@@ -0,0 +1,10 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(`This is ${i} incrementation`)
+// }
+
+
+//Perfect square loop. Perfect sqaure is a number * number up to 20. So 1*1, 2*2, 3*3 and so on
+
+for(let i=1; i<=20; i++){
+    console.log(i*i)
+}
---
+++ Loops/2_loops_in_arrays.js
@@ -0,0 +1,4 @@
+const examScores = [12, 45, 32, 88, 92, 87, 65, 100, 55]
+for(let i = 0; i <  examScores.length ; i++){
+    console.log(sort(`Score of a student: ${examScores[i]}`))
+}
