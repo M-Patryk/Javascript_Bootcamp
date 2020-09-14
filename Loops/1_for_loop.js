51fbc836975c725f9feae9dc3db5712cac4463ea
PatrykM <patrykxmajchrzak@gmail.com>
2020-09-14 21:59:02 +0200

    playing with loops

--- Loops/1_for_loop.js
+++ Loops/1_for_loop.js
@@ -2,9 +2,8 @@
 //     console.log(`This is ${i} incrementation`)
 // }
 
-
 //Perfect square loop. Perfect sqaure is a number * number up to 20. So 1*1, 2*2, 3*3 and so on
 
-for(let i=1; i<=20; i++){
-    console.log(i*i)
+for (let i = 1; i <= 20; i++) {
+	console.log(i * i);
 }
--- Loops/2_loops_in_arrays.js
+++ Loops/2_loops_in_arrays.js
@@ -1,4 +1,37 @@
-const examScores = [12, 45, 32, 88, 92, 87, 65, 100, 55]
-for(let i = 0; i <  examScores.length ; i++){
-    console.log(sort(`Score of a student: ${examScores[i]}`))
+// const examScores = [ 12, 45, 32, 88, 92, 87, 65, 100, 55 ];
+// for (let i = 0; i < examScores.length; i++) {
+// 	console.log(`Score of a student: ${examScores[i]}`);
+// }
+
+const myStudents = [
+	{
+		firstName : 'Patryk',
+		grade     : 88
+	},
+	{
+		firstName : 'Gabi',
+		grade     : 100
+	},
+	{
+		firstName : 'Kondi',
+		grade     : 66
+	},
+	{
+		firstName : 'Ewa',
+		grade     : 32
+	}
+];
+
+for (let i = 0; i < myStudents.length; i++) {
+	console.log(`Student:${[ i + 1 ]} ${myStudents[i].firstName} // Grade:${myStudents[i].grade}`);
+	//Lub cos takiego
+	// let student = myStudents[i];
+	// console.log(`${student.firstName} ${student.grade}`) // <-- Lepsza opcja imo
 }
+
+const word = 'somelongword';
+let backWord = ''
+for (let j = word.length - 1; j >= 0; j--) {
+    backWord += word[j];
+}
+console.log(backWord)
