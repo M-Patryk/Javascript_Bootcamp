49c7732f493a02fa578f9c7b3b3b62c0a0665594
PatrykM <patrykxmajchrzak@gmail.com>
2020-09-15 20:07:13 +0200

    while loops with examples

--- Loops/4_while_loop.js
+++ Loops/4_while_loop.js
@@ -4,3 +4,18 @@ while (num <= 5) {
 	console.log(num);
 	num++;
 }
+
+//We usually use "while loop" if we dont know exactly how many times we will run it
+//Like the 2048 game. You move square till there is no more moves left
+
+//Guess game as an example
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+let checkingNumber = Math.floor(Math.random() * 10 + 1);
+let counter = 0
+while (checkingNumber !== randomNumber) {
+	checkingNumber = Math.floor(Math.random() * 10 + 1);
+    console.log(`Random number: ${randomNumber} // Checking number: ${checkingNumber}`);
+    counter++;
+}
+console.log('This is over. You win!')
+console.log(`Tries to find the correct number: ${counter}`)
