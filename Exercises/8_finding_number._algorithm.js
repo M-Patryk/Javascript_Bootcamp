1915839e050adc31c948a7533476d4b5aabc0e2a
PatrykM <patrykxmajchrzak@gmail.com>
2020-09-24 10:31:28 +0200

    finding a number either by adding 5 or multiplying it by 3

---
+++ Exercises/8_finding_number._algorithm.js
@@ -0,0 +1,16 @@
+function findNumber(number) {
+	function checkingForSolution(current, history) {
+		if (current == number) {
+			return history;
+		} else if (current > number) {
+			return null;
+		} else {
+			return (
+				checkingForSolution(current + 5, `(${history} + 5)`) ||
+				checkingForSolution(current * 3, `(${history} * 3)`)
+			);
+		}
+	}
+
+	return checkingForSolution(1, '1');
+}
