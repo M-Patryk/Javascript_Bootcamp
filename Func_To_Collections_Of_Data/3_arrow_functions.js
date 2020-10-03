258fee263dfd541282feb10037c9b15b8c44105a
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-03 09:41:14 +0200

    arrow funtions - more practice

--- Func_To_Collections_Of_Data/3_arrow_functions.js
+++ Func_To_Collections_Of_Data/3_arrow_functions.js
@@ -1,14 +1,35 @@
 //Basically a shortcut to create functions.
 
 //Longer version
-const longer = function(x){
-    return x
-}
+const longer = function(x) {
+	return x;
+};
 
 //Shorter version (nfn)
 const shorter = (x) => {
-    return x
-}
+	return x;
+};
 
+// Even shorter (works only with one condition and simple statements (return x))
+const square = (x) => x * x;
 
-//nfn anfn
+const nums = [ 2, 4, 6, 8, 10, 12 ];
+const doubles = nums.map((num) => {
+	return num * 2;
+});
+
+//Shorter version
+const doubles2 = nums.map((numb) => numb * 2);
+
+//Writing more functions to get used to syntax
+const triple = nums.map((num) => {
+	return num * 3;
+});
+
+const quadruple = nums.map((num) => {
+	return num * 3;
+});
+
+const quintuple = nums.map(num => {
+    return num * 5
+})
