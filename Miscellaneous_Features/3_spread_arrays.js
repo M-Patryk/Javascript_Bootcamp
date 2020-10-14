dc662cb03288b3d5b6eaf4a5ce9d17dded7c2403
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-14 20:12:46 +0200

    spread objects and added note to spread_arrays

--- Miscellaneous_Features/3_spread_arrays.js
+++ Miscellaneous_Features/3_spread_arrays.js
@@ -7,3 +7,7 @@ const reptiles = ['lizard', 'crocodile', 'frog', 'cameleon']
 //Mozna tez dodawac w "trakcie" tworzenia np
 
 const animals = [...birds, 'lion', ...mammals, 'turtle', ...reptiles]
+
+//We can copy array BUT ITS ONLY 1 LEVEL DEEP. What it means is that we wont copy nested arrays
+nestedAnimals = ['spider', ['hairy fright', ['badboy']]]
+const array = [...mammals, ...nestedAnimals]
--- Miscellaneous_Features/4_spread_objects.js
+++ Miscellaneous_Features/4_spread_objects.js
@@ -1,2 +1,52 @@
 //  Feline - relating to cats || Canine - relating to dogs
+//We can't copy nested arrays
+const feline = {
+    legs: 4,
+    family: 'Felidae'
+}
 
+const canine = {
+    furry: true,
+    family: 'Caninae'
+}
+
+
+//Example
+const dog = {
+    ...canine,
+    isPet: true,
+    isAdorable: true
+}
+console.log('dog', dog)
+
+const catDog = {
+    ...canine,
+    ...feline
+}
+//Another example (order matters) If we had object with 3 legs and we copied object that has 4 legs it would override the "3 leg object"
+//So to avoid such a incidents we have to override legs at the end
+const triLeg = {
+    ...feline,
+    legs: 3,
+    isFluffy: true
+}
+console.log('triLeg', triLeg)
+
+
+//We can copy variables, like in array example
+const catDogClone = {
+    ...catDog
+}
+console.log('catDog', catDog)
+
+//We can spread array or string into object
+console.log('spread array into object', {
+    ...[4, 5, 6]
+})
+console.log('spread string into object', {
+    ...'qwerty'
+})
+
+//We can also spread objects into arrays
+const randomStuff = [...'somearray', {...catDog}]
+console.log(randomStuff)
