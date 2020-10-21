42eca3ee441b1cf6e79a3c10c30014f1b77ca72a
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-21 18:38:10 +0200

    added draw multiple cards function

--- Exercises/12_deck_of_cards.js
+++ Exercises/12_deck_of_cards.js
@@ -12,7 +12,7 @@
 
 // const myDeck = makeDeck();
 
-// function drawCard(){
+// function takeCard(){
 //     const randomCard = Math.floor(Math.random() * myDeck.length)
 //     return myDeck.pop()
 // }
@@ -21,23 +21,30 @@
 //It would be way better to do all of it in an object
 
 const myDeck = {
-	values     : '2,3,4,5,6,7,8,9,10,J,Q,K,A',
-	suits      : [ 'hearts', 'diamonds', 'clubs', 'spades' ],
-	deck       : [],
-	drawnCards : [],
+	values            : '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+	suits             : [ 'hearts', 'diamonds', 'clubs', 'spades' ],
+	deck              : [],
+	drawnCards        : [],
 	makeDeck() {
 		const { values, suits, deck } = this;
 		for (let value of values.split(',')) {
 			for (let suit of suits) {
 				deck.push({ value, suit });
 			}
-        }
-        return deck.sort(() => Math.random() - 0.5);
+		}
+		return deck.sort(() => Math.random() - 0.5);
 		// return deck; it this case (when function and deck variable are in objects) we don't have to return deck
 	},
-	drawCard() {
-        const drawnCard = this.deck.pop();
-        this.drawnCards.push(drawnCard)
+	takeCard() {
+		const drawnCard = this.deck.pop();
+		const draw = this.drawnCards.push(drawnCard);
 		return drawnCard;
+	},
+	drawMultipleCards(quantity=1) {
+		const cards = [];
+		for (let i = 0; i < quantity; i++) {
+			cards.push(this.takeCard())
+		}
+		return cards
 	}
 };
