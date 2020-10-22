8dc54d3f8436b110069d741ae3dd38dd1b03b3ac
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-22 20:24:26 +0200

    added info about querySelector

--- DOM_Manipulation/1_selecting.js
+++ DOM_Manipulation/1_selecting.js
@@ -6,6 +6,14 @@
 // document.getElementByTagName('ul')
 // document.getElementByClassName('special')
 
+//It's good to note, that if i write document.getElementsById, it will get ALL elements (added "s" to "element")
+
 
 //With variable
 //const flipBear = document.getElementById('bear-photo)
+
+
+//There is actually 4th method which "rules them all"
+// --- querySelector && querySelectorAll
+//The reason the "querySelectorAll" exists is because "querySelector" selects ONLY ONE element
+
