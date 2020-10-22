8322f1e3804602722a74522ff11550926f949fe1
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-22 13:48:25 +0200

    getElement stuff. 'it grabs' specific elements

---
+++ DOM_Manipulation/1_selecting.js
@@ -0,0 +1,11 @@
+//Nothing really special here. Just a few methods that i have to remember.
+//Methods below "take" specific element and put it into variable if we define one
+//One thing to remember is that the values have to be put as a strings
+
+// document.getElementById('bear-photo') 
+// document.getElementByTagName('ul')
+// document.getElementByClassName('special')
+
+
+//With variable
+//const flipBear = document.getElementById('bear-photo)
