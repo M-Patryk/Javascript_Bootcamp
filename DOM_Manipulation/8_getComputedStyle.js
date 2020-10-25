6dc9a0ec3776bdc00fbef49c4baf7daed4ffc71a
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-25 13:13:36 +0100

    getComputedStyle - returns CSS properties

---
+++ DOM_Manipulation/8_getComputedStyle.js
@@ -0,0 +1,5 @@
+//It retrives what the current styling value for the color, display, position etc. is
+const li = document.querySelector('li')
+
+//It will return the CSS Style Declaration (css values of HTML elements)
+const liStyles = getComputedStyle(li)
