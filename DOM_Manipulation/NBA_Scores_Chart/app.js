f57eeca0e4f0c171be366d93857c0d5e6b539815
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-28 18:09:04 +0100

    Refactored code and few updates Updates: added section HTML elements changed li style to none in CSS

--- DOM_Manipulation/NBA_Scores_Chart/app.js
+++ DOM_Manipulation/NBA_Scores_Chart/app.js
@@ -84,6 +84,17 @@ const warriorsGames = [
 		}
 	}
 ];
+// {
+// 	awayTeam : {
+// 		team     : 'Golden State',
+// 		points   : 119,
+// 		isWinner : true
+// 	},
+// 	homeTeam : {
+// 		team     : 'Houston',
+// 		points   : 106,
+// 		isWinner : false
+// 	}
 
 //Code
 //Napisac kod aby wyswietlal w liscie:
@@ -91,41 +102,85 @@ const warriorsGames = [
 //--jaki byl wynik (lepszy wynik pogrubiona czcionka)
 //--kolor zielony jesli wygrali "Golden State", czerwony jesli nie
 
-//----------First we have to display games somehow ----------
-// We create 'ul' to put games inside it
-const parentUl = document.createElement('ul');
-// Using loop we add all games to "ul" as 'li'
-for (let game of warriorsGames) {
-	//Destructuring so we don't have to write game.awayTeam, game.homeTeam etc
-	const { awayTeam, homeTeam } = game;
+//REFACTORED CODE - Info how things work below!
+
+const makeChart = (games, targetTeam) => {
+	const parentUl = document.createElement('ul');
+	for (let game of games) {
+		const gameLi = document.createElement('li');
+		gameLi.innerHTML = getInfo(game);
+		gameLi.classList.add(checkForWinner(game, targetTeam) ? 'win' : 'loss');
+		parentUl.append(gameLi);
+	}
+	return parentUl;
+};
+//Function priting info
+const getInfo = ({ homeTeam, awayTeam }) => {
 	const { team: aTeam, points: aPoints } = awayTeam;
 	const { team: hTeam, points: hPoints } = homeTeam;
 
-	//Li element in which games are going to be put in
-	const gameLi = document.createElement('li');
-
-	//What every 'li' should show
 	const teamNames = `${aTeam} @ ${hTeam}`;
 
-	//If zeby pogrubialo wynik, ktory jest wiekszy. Przed ifem trzeba zadeklarowac teamScores jako LET, bo inaczej jest problem ze "scope"
 	let teamScores;
 	if (aPoints > hPoints) {
 		teamScores = `<b>${aPoints}</b>-${hPoints}`;
 	} else {
 		teamScores = `${aPoints}-<b>${hPoints}</b>`;
 	}
+	return `${teamNames} ${teamScores}`;
+};
 
-	//Ustawianie koloru (CSS class) Jesli "Golden State" wygrywa ma byc zielony, jesli przegrywa czerwony
-	//Nowa zmienna, ktora sprawdza czy hTeam to "Golden State". Jesli tak to warriors = homeTeam (object key), jesli nie jest
-	//"Golden State" to musi byc ten drugi "Golden State", czyli warriors = awayTeam (i znowu odniesienie do obiektu)
-	const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
+//Checking for winner function
+const checkForWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+	const winner = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+	return winner.isWinner;
+};
 
-	//We add specified info as 'li'
-	gameLi.classList.add(warriors.isWinner ? 'win' : 'loss');
-	gameLi.innerHTML = `${teamNames} ${teamScores}`;
+//Printing stuff on website
+const gsSection = document.querySelector('#gs');
+const hrSection = document.querySelector('#hr');
+const gsChart = makeChart(warriorsGames, 'Golden State');
+const hrChart = makeChart(warriorsGames, 'Houston');
+gsSection.append(gsChart);
+hrSection.append(hrChart);
 
-	//And at the end we add every 'li' we created to 'ul'
-	parentUl.append(gameLi);
-}
-//Here i add 'ul' to HTML so it shows up on site
-document.body.prepend(parentUl);
+//Prefactored code
+
+//----------First we have to display games somehow ----------
+// // We create 'ul' to put games inside it
+// const parentUl = document.createElement('ul');
+// // Using loop we add all games to "ul" as 'li'
+// for (let game of warriorsGames) {
+// 	//Destructuring so we don't have to write game.awayTeam, game.homeTeam etc
+// 	const { awayTeam, homeTeam } = game;
+// 	const { team: aTeam, points: aPoints } = awayTeam;
+// 	const { team: hTeam, points: hPoints } = homeTeam;
+
+// 	//Li element in which games are going to be put in
+// 	const gameLi = document.createElement('li');
+
+// 	//What every 'li' should show
+// 	const teamNames = `${aTeam} @ ${hTeam}`;
+
+// 	//If zeby pogrubialo wynik, ktory jest wiekszy. Przed ifem trzeba zadeklarowac teamScores jako LET, bo inaczej jest problem ze "scope"
+// 	let teamScores;
+// 	if (aPoints > hPoints) {
+// 		teamScores = `<b>${aPoints}</b>-${hPoints}`;
+// 	} else {
+// 		teamScores = `${aPoints}-<b>${hPoints}</b>`;
+// 	}
+
+// 	//Ustawianie koloru (CSS class) Jesli "Golden State" wygrywa ma byc zielony, jesli przegrywa czerwony
+// 	//Nowa zmienna, ktora sprawdza czy hTeam to "Golden State". Jesli tak to warriors = homeTeam (object key), jesli nie jest
+// 	//"Golden State" to musi byc ten drugi "Golden State", czyli warriors = awayTeam (i znowu odniesienie do obiektu)
+// 	const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
+
+// 	//We add specified info as 'li'
+// 	gameLi.classList.add(warriors.isWinner ? 'win' : 'loss');
+// 	gameLi.innerHTML = `${teamNames} ${teamScores}`;
+
+// 	//And at the end we add every 'li' we created to 'ul'
+// 	parentUl.append(gameLi);
+// }
+// //Here i add 'ul' to HTML so it shows up on site
+// document.body.prepend(parentUl);
