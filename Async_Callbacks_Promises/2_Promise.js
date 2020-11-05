dfdbdbab20876e3ef1964052577ec53af0734e9b
PatrykM <patrykxmajchrzak@gmail.com>
2020-11-05 14:50:20 +0100

    Added timing to the promise

--- Async_Callbacks_Promises/2_Promise.js
+++ Async_Callbacks_Promises/2_Promise.js
@@ -1,17 +1,22 @@
 // Jak sama nazwa wskazuje, jest to funkcja, ktora sie wykonywana w danym przypadku.
 // .then() wykonuje dla RESOLVE
 // .catch() wykonuje dla REJECT
-const willGetADog = new Promise((resolve, reject) => {
-	const rand = Math.random();
-	if (rand > 0.5) {
-		resolve();
-	} else {
-		reject();
-	}
-});
-
-willGetADog.then(() => {
-    console.log('YAY I GOT A DOG!')
-}).catch(() => {
-    console.log('No Dog')
-})
+const getADog = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const rand = Math.random();
+			if (rand > 0.5) {
+				resolve();
+			} else {
+				reject();
+			}
+		}, 3000);
+	});
+};
+getADog()
+	.then(() => {
+		console.log('YAY I GOT A DOG!');
+	})
+	.catch(() => {
+		console.log('No Dog');
+	});
