72584ecd733a39d89e8b4f3a0201e85dbcbd0a6e
Patryk <47945758+M-Patryk@users.noreply.github.com>
2020-11-18 11:16:29 +0100

    Added tick function which counts down the timer as well as start and stop functions

--- PART_2_Projects/Timer_project/app.js
+++ PART_2_Projects/Timer_project/app.js
@@ -1,18 +1,32 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton){
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+	constructor(durationInput, startButton, pauseButton) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener('click', this.start)
-    }
-    start(){
-        console.log('Timer started!')
-    }
+        this.durationInput.value = 30
+		this.startButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
+	}
+	start = () => {
+		this.tick();
+        this.interval = setInterval(this.tick, 1000);
+	};
+	tick = () => {
+        console.log('tick');
+        this.durationInput.value -= 1
+        if(this.durationInput.value == 0){
+            this.pause()
+            alert("Time is over!")
+        }
+	};
+	pause = () => {
+		clearInterval(this.interval);
+	};
 }
 
-const durationInput = document.querySelector('#duration')
-const startButton = document.querySelector('#start')
-const pauseButton = document.querySelector('#pause')
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton);
