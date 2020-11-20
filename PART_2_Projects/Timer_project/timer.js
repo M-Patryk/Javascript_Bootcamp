abc2968564d3a9a10a791b8f2631dc17c5870c28
Patryk <47945758+M-Patryk@users.noreply.github.com>
2020-11-20 11:54:43 +0100

    Made the timers border animate with specific time values

--- PART_2_Projects/Timer_project/timer.js
+++ PART_2_Projects/Timer_project/timer.js
@@ -16,27 +16,24 @@ class Timer {
 	}
 	start = () => {
 		if (this.onStart) {
-			this.onStart();
+			this.onStart(this.timeRemaining);
 		}
 		this.tick();
-		this.interval = setInterval(this.tick, 1000);
-		if (this.timeRemaining <= 0) {
-			this.pause()
-		}
+		this.interval = setInterval(this.tick, 20);
 	};
 	pause = () => {
 		clearInterval(this.interval);
-		if (this.onComplete) {
-			this.onComplete();
-		}
 	};
 	tick = () => {
 		if (this.timeRemaining <= 0) {
 			this.pause();
+			if (this.onComplete) {
+				this.onComplete();
+			}
 		} else {
-			this.timeRemaining = this.timeRemaining - 1;
+			this.timeRemaining = this.timeRemaining - 0.02;
 			if (this.onTick) {
-				this.onTick();
+				this.onTick(this.timeRemaining);
 			}
 		}
 	};
@@ -44,6 +41,6 @@ class Timer {
 		return parseFloat(this.durationInput.value);
 	}
 	set timeRemaining(time) {
-		this.durationInput.value = time;
+		this.durationInput.value = time.toFixed(2);
 	}
 }
